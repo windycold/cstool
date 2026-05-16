4b750d9e03e7791439854ea3d1ad269797f2ce18
windy <lsl2660414256@163.com>
2026-05-17 00:12:12 +0800

    pip换源初实现

--- src/recipes/pip.rs
+++ src/recipes/pip.rs
@@ -1,4 +1,5 @@
 use crate::core::{MirrorError, MirrorManager, MirrorSite, Scope};
+use std::{io::Error, process::Command};
 
 pub const PIP: MirrorManager = MirrorManager {
     name: "pip",
@@ -8,11 +9,23 @@ pub const PIP: MirrorManager = MirrorManager {
     mirrors: &[MirrorSite {
         name: "tuna",
         description: "",
-        url: "",
-        test_url:"",
+        url: "https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple",
+        test_url: "",
     }],
     set_fun: pip_set,
 };
-fn pip_set(mirror:&MirrorSite,scope: Option<Scope>)->Result<(), MirrorError> {
-    todo!()
+fn pip_set(mirror: &MirrorSite, _: Option<Scope>) -> Result<(), MirrorError> {
+    let output = Command::new("pip")
+        .arg("config")
+        .arg("set")
+        .arg("global.index-url")
+        .arg(mirror.url)
+        .output()?;
+    if !output.status.success() {
+        return Err(MirrorError::Io(Error::other(format!(
+            "pip 命令执行失败: {}",
+            String::from_utf8_lossy(&output.stderr)
+        ))));
+    };
+    Ok(())
 }
